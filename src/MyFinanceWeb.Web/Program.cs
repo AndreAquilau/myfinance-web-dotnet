4971360ace92f8de67a0b09244d0be9eb5b74ddc
andreaquilau <andre.aquilau@outlook.com>
2023-11-24 00:09:23 -0400

    Services

--- src/MyFinanceWeb.Web/Program.cs
+++ src/MyFinanceWeb.Web/Program.cs
@@ -1,9 +1,16 @@
 using MyFinanceWeb.Infra.IoC;
+using MyFinanceWeb.Application;
+using AutoMapper;
+using Castle.Core.Smtp;
+using System.Reflection;
+using MyFinanceWeb.Application.Profiles;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAutoMapper(typeof(PlanoContaProfile));
+builder.Services.AddAutoMapper(typeof(TransacaoProfile));
 builder.Services.AddInfrastructure(configuration: builder.Configuration);
 
 var app = builder.Build();
